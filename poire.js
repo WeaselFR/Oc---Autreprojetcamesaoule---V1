a77e2ae82331c829a43b37b5ded8605a59526b49
WeaselFR <benoit.rogeau@laposte.net>
2023-07-01 20:35:20 +0200

    V7

---
+++ poire.js
@@ -0,0 +1,29 @@
+const token = localStorage.getItem("token");
+if (token) {
+  window.location.href = "index.html";
+}
+
+const Jesus = document.querySelector("#Jesus");
+
+Jesus.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = formLogin.email.value;
+  const password = formLogin.password.value;
+
+fetch("http://localhost:5678/api/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ email, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        window.location.href = "index.html";
+      } else {
+        alert("Erreur dans l'identifiant ou le mot de passe");
+      }
+    });
+});
