b6a3a2fb9ec08dd0d143d992b96135bda45dc8a2
WeaselFR <benoit.rogeau@laposte.net>
2023-07-08 12:50:15 +0200

    V30 TEST

--- javascript/gallery.js
+++ javascript/gallery.js
@@ -1,40 +1,123 @@
-const modal = document.querySelector("#modal");
-
 const gallery = document.querySelector(".gallery");
+const dataStorage = localStorage.getItem("token");
+const unlog = document.querySelector(".logout");
+const buttonFiltres = document.querySelector(".filters");
+const divModifier = document.querySelectorAll("button.modifier");
+const divEdition = document.querySelector(".editionMode");
 
 let data = [];
 
 const getWorks = (id) => {
-    fetch("http://localhost:5678/api/works")
-      .then(response => response.json())
-      .then(dataJson => {
-        const dataForGallery = id === 0 ? dataJson : dataJson.filter(item => item.categoryId === id);
-        addWorksGallery(dataForGallery);
-        const buttons = document.getElementsByClassName("btn");
-        Array.from(buttons).forEach(button => {
-          const buttonId = parseInt(button.id);
-          if (buttonId === id) {
-            button.classList.add("button-active");
-          } else {
-            button.classList.remove("button-active");
-          }
-        });
-        if (modal.style.display === "block") {
-          AddWorksGalleryModale(dataJson);
+  fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(dataJson => {
+      const dataForGallery = id === 0 ? dataJson : dataJson.filter(item => item.categoryId === id);
+      addWorksGallery(dataForGallery);
+      const buttons = document.getElementsByClassName("btn");
+      Array.from(buttons).forEach(button => {
+        const buttonId = parseInt(button.id);
+        if (buttonId === id) {
+          button.classList.add("button-active");
+        } else {
+          button.classList.remove("button-active");
         }
       });
-  };
+      const modal = document.querySelector(".modal"); 
+      if (modal.style.display === "block") {
+        AddWorksGalleryModale(dataJson);
+      }
+    });
+};
 
-//La première fonction .then est utilisée pour traiter la réponse de la requête et la convertir en JSON.
-//La deuxième fonction .then est utilisée pour manipuler les données JSON récupérées.
-//Les données JSON converties sont stockées dans une variable dataJson.
-//Une autre variable appelée dataForGallery est initialisée comme un tableau vide.
-//Si l'id passé en paramètre est égal à 0, alors toutes les données JSON sont ajoutées à dataForGallery. 
-//Sinon, la boucle for est utilisée pour parcourir les données JSON et que les œuvres ayant un categoryId égal à l'id passé en paramètre sont ajoutées à dataForGallery.
-//La fonction addWorksGallery est appelée avec dataForGallery en argument pour afficher les œuvres dans la galerie.
-//Ensuite, une sélection des éléments HTML avec la classe "btn" est effectuée et stockée dans la variable buttons.
-//Une boucle forEach est utilisée pour parcourir chaque bouton dans buttons.
-//Si l'id du bouton converti en entier est égal à l'id passé en paramètre, alors la classe "button-active" est ajoutée au bouton. Sinon, la classe "button-active" est supprimée du bouton.
-//Si la valeur de modal.style.display est "block" (si le modal est affiché), alors la fonction AddWorksGalleryModale est appelée avec les données JSON (dataJson) en argument.
+getWorks(0);
 
-// "array" est une structure de données utilisée pour stocker et organiser une collection d'éléments
+function addWorksGallery(data) {
+  let child = gallery.lastElementChild;
+  while (child) {
+    gallery.removeChild(child);
+    child = gallery.lastElementChild;
+  }
+
+  data.forEach((element) => {
+    const figure = `<figure id=${element.id}Gallery>
+      <img src=${element.imageUrl} alt=${element.title}>
+      <figcaption>${element.title}</figcaption>
+    </figure>`;
+    gallery.innerHTML += figure;
+  });
+}
+
+let saveCategori = [];
+
+const allButton = document.querySelector(".filters");
+
+const getCategories = () => {
+  fetch("http://localhost:5678/api/categories")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (dataCategories) {
+      for (let i = 0; i < dataCategories.length; i++) {
+        if (dataCategories[i].id > 0) {
+          saveCategori.push(dataCategories[i]);
+        }
+      }
+
+      addButton(dataCategories);
+    })
+    .catch(() => {
+      console.log("Erreur lors de la récupération des catégories");
+    });
+
+  function addButton(categories) {
+    categories.push({ id: 0, name: "Tous" });
+
+    categories.sort((a, b) => a.id - b.id);
+
+    categories.forEach((categorie) => {
+      const button = document.createElement("button");
+
+      if (categorie.id === 0) {
+        button.className = "button-active btn";
+      } else {
+        button.className = "btn";
+      }
+
+      button.id = categorie.id;
+
+      button.innerHTML = categorie.name;
+
+      button.addEventListener("click", () => filtres(categorie.id));
+
+      allButton.appendChild(button);
+    });
+  }
+};
+
+getCategories();
+
+function filtres(id) {
+  getWorks(id);
+}
+
+if (dataStorage) {
+  unlog.textContent = "logout";
+
+  buttonFiltres.style.display = "none";
+
+  gallery.style = "margin-top: 90px;";
+
+  divEdition.style.display = "flex";
+
+  divModifier.forEach((div) => {
+    div.style.display = "flex";
+  });
+
+  unlog.addEventListener("click", () => {
+    localStorage.removeItem("token");
+    unlog.href = "index.html";
+  });
+
+  const modal = document.querySelector(".modal");
+  modal.style.display = "none";
+}
